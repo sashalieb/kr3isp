7577a24422516694b69a564592e750df876baddc
sashalieb <sashalieb@mail.ru>
2024-05-08 11:50:21 +0500

    исправленный

---
+++ src/main/java/org/example/PhoneBook.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneBook {
+    private Map<String, Set<String>> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    public void addContact(String name, String phoneNumber) {
+        Set<String> numbers = phoneBook.getOrDefault(name, new HashSet<>());
+        numbers.add(phoneNumber);
+        phoneBook.put(name, numbers);
+    }
+
+    public Set<String> findContact(String name) {
+        return phoneBook.get(name);
+    }
+
+    public void displayContacts() {
+        for (Map.Entry<String, Set<String>> entry : phoneBook.entrySet()) {
+            System.out.println("Имя: " + entry.getKey() + ", Номер телефона: " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addContact("Алиса", "1234567890");
+        phoneBook.addContact("Алиса", "0987654321");
+        phoneBook.addContact("Владимир", "1112223333");
+
+        System.out.println("Все контакты:");
+        phoneBook.displayContacts();
+
+        System.out.println("\nНомера Алисы:");
+        Set<String> johnNumbers = phoneBook.findContact("Алиса");
+        if (johnNumbers != null) {
+            for (String number : johnNumbers) {
+                System.out.println(number);
+            }
+        } else {
+            System.out.println("Номер не найден");
+        }
+    }
+}
+
