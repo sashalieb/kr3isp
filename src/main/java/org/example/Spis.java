1749d786ff5fda35dc91b7d6832b51e3a22c8269
sashalieb <151907081+sashalieb@users.noreply.github.com>
2024-05-08 12:17:31 +0500

    Add files via upload

--- src/main/java/org/example/Spis.java
+++ src/main/java/org/example/Spis.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Spis {
     // Метод для перестановки элементов списка так, чтобы сначала шли числа, не превосходящие X,
     // а затем числа, большие X
-    public static void rearrange(List<Integer> list, int x) {
+    public static void perestanovka (List<Integer> list, int x) {
 
         int n = 0;
         int m = list.size() - 1;
@@ -30,14 +30,12 @@ public class Spis {
     }
 
     public static void main(String[] args) {
-        // Исходный список
-        List<Integer> list = new ArrayList<>(List.of(9, 8, 7, 6, 5, 4, 3, 2, 1));
         // Число X
         int x = 5;
-
+        // Исходный список
+        List<Integer> list = new ArrayList<>(List.of(9, 8, 7, 6, 5, 4, 3, 2, 1));
         // Вызов метода для перестановки
-        rearrange(list, x);
-
+        perestanovka(list, x);
         // Вывод результата
         System.out.println("Полученный список:");
         for (int num : list) {
